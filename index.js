314eeb420be50c246e479d31d63f8de543c83cc2
Juan Guerra <juancm1823@hotmail.com>
2022-07-28 20:45:44 -0500

    'prurbas'

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
-const {app, BrowserWindow} = require ('electron')
-const devtools = require('./devtools')
+const { app, BrowserWindow } = require("electron");
+const devtools = require("./devtools");
+const axios = require("axios").default;
+const os=require('os')
 
-if(process.env.NODE_ENV ==='development'){
 
-    devtools.run_dev_tools()
+if (process.env.NODE_ENV === "development") {
+  devtools.run_dev_tools();
 }
 
+app.on("ready", () => {
+  let win = new BrowserWindow({
+    width: 400,
+    height: 400,
+  });
+  win.loadFile("index.html");
+  win.toggleDevTools();
 
-app.on('ready',()=>{
-    let win =new BrowserWindow({
-        width:400,
-        height:400
+  let timer = setInterval(() => {
+    if (win.isVisible()) {
+      win.focus();
+    } else {
+      win.show();
+    }
+  }, 5000);
+
+  axios
+    .get("https://jsonplaceholder.typicode.com/users")
+    .then((res) => {
+     // console.log(res.data)
+    }).catch((error)=>{
+        console.log(error)
     })
-    win.loadFile('index.html')
-    win.toggleDevTools()
+
+    console.log(os.userInfo())
     
-})
+});
